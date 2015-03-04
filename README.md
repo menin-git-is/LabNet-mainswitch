5980ec058e1ee459c0f96336df03084b9b240a99
morbidick <mail@morbi-happens.de>
2015-03-04 20:25:59 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Temporary Main Switch for FabLab Karlsruhe
+## run
+````
+sudo modprobe i2c-dev
+sudo apt-get install python-request
+sudo cp main.py /etc/init.d/labnet-mainswitch
+sudo chmod +x /etc/init.d/labnet-mainswitch
+sudo update-rc.d labnet-mainswitch defaults
+
+````
+
+## circuit diagram
+````
+5V ____/ _____GPIO 14 (PIN 8)
+          |
+          |_
+          | |10k
+          |_|
+          |
+          o GND
+````
---
+++ main.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+
+import sys
+import requests
+import RPi.GPIO as GPIO
+import time
+
+print sys.argv
+
+if ( sys.argv[1] != "start" ):
+  print "this is not a startup, gonna exit"
+  sys.exit()
+
+switch_pin = 14
+switch_delay = 0.5
+prev_input = 0
+labnet_url = "http://labnet.lab.flka.de"
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(switch_pin,GPIO.IN)
+
+def toggle_lab(state):
+  if ( state == 0 ):
+    print requests.get(labnet_url + "/api/lab/off")
+  else:
+    print requests.get(labnet_url + "/api/lab/on")
+  return
+
+while True:
+  input = GPIO.input(switch_pin)  
+  if ( prev_input != input ):
+    toggle_lab(input)
+  prev_input = input
+  time.sleep(switch_delay)
+
