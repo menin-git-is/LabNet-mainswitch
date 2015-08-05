9933029e3a4f04181d18107fe5358790ace897d9
morbidick <mail@morbi-happens.de>
2015-08-05 22:31:04 +0000

    added error handling

--- main.py
+++ main.py
@@ -13,12 +13,24 @@ labnet_url = "http://labnet.lab.flka.de"
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(switch_pin,GPIO.IN)
 
+def query_api(arg):
+  try:
+    requests.get(labnet_url + "/api/" + arg)
+  except requests.exceptions.RequestException as e:
+    print "http request failed:"
+    print e
+  else:
+    print "http request succeeded: " + arg
+  return
+
 def toggle_lab(state):
   if ( state == 0 ):
-    print requests.get(labnet_url + "/api/lab/off")
+    query_api("lab/off")
   else:
-    print requests.get(labnet_url + "/api/lab/on")
+    query_api("lab/on")
   return
+
+print "daemon started"
 
 while True:
   input = GPIO.input(switch_pin)  
