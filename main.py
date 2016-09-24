1212ec64c7e2e74e6dd1b27380988c170333c6cf
Vikath <000_pc@gmx.net>
2016-09-24 15:58:25 +0200

    Added logging and debouncing

--- main.py
+++ main.py
@@ -4,15 +4,21 @@ import sys
 import requests
 import RPi.GPIO as GPIO
 import time
+import logging
 
 switch_pin = 14
 switch_delay = 0.5
 prev_input = -1
+input_counter = 0
+needed_inputs_to_change = 5
 labnet_url = "http://192.168.1.6"
 state_api_url = "http://192.168.1.6:3000"
+LOG_FILENAME = "/var/log/labnet_mainswitch.log"
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(switch_pin,GPIO.IN)
+
+logging.basicConfig(filename=LOG_FILENAME, format='%(asctime)s %(message)s', level=logging.DEBUG)
 
 def query_api(arg):
   try:
@@ -23,10 +29,10 @@ def query_api(arg):
     else:
       requests.post(state_api_url, json={"is_open":False}, headers={'clientId':'supersecret'})
   except requests.exceptions.RequestException as e:
-    print "http request failed:"
+    logging.info("http request failed:")
     print e
   else:
-    print "http request succeeded: " + arg
+    logging.info("http request succeeded: " + arg)
   return
 
 def toggle_lab(state):
@@ -36,13 +42,16 @@ def toggle_lab(state):
     query_api("lab/on")
   return
 
-print "daemon started"
+logging.info("daemon started")
 
 while True:
   input = GPIO.input(switch_pin)  
   if ( prev_input != input ):
-    toggle_lab(input)
-    prev_input = input
+    if(input_counter++ >= needed_inputs_to_change)
+      toggle_lab(input)
+      prev_input = input
+  else
+    input_counter = 0
   time.sleep(switch_delay)
 
 
