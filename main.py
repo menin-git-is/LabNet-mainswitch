a47ec0791e39df1a29d66f2e7c8972e7d6a6ad06
Sven Hecht <sven.hecht@citrix.com>
2017-08-09 19:28:10 +0200

    Refactor to remove last PEP8 issues

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ PEN_SWITCH_PIN = 15
 BUZZER_PIN = 18
 LABNET_URL = "http://192.168.1.6"
 STATE_API_URL = "http://192.168.1.6:3000"
+STATE_API_HEADERS = {'clientId': 'supersecret'}
 
 
 class DebouncedInput:
@@ -56,35 +57,34 @@ class Main:
         GPIO.setup(PEN_SWITCH_PIN, GPIO.IN)
         GPIO.setup(BUZZER_PIN, GPIO.OUT)
 
-        logging.basicConfig(filename=LOG_FILENAME, format='%(asctime)s %(message)s', level=logging.DEBUG)
+        logging.basicConfig(
+            filename=LOG_FILENAME,
+            format='%(asctime)s %(message)s',
+            level=logging.DEBUG)
 
-    def send_update(self, arg):
+    def send_update(self, state):
         """send a state update to the API."""
 
         try:
-            requests.get(LABNET_URL + "/api/" + arg)
+            requests.get(LABNET_URL + "/api/" + state)
 
-            if arg == "lab/on":
-                requests.post(STATE_API_URL, json={"is_open": True}, headers={
-                              'clientId': 'supersecret'})
+            if state == "lab/on":
+                requests.post(STATE_API_URL, json={"is_open": True}, headers=STATE_API_HEADERS)
             else:
-                requests.post(STATE_API_URL, json={"is_open": False}, headers={
-                              'clientId': 'supersecret'})
+                requests.post(STATE_API_URL, json={"is_open": False}, headers=STATE_API_HEADERS)
         except requests.exceptions.RequestException as ex:
             logging.info("http request failed:")
             logging.error(ex)
         else:
-            logging.info("http request succeeded: " + arg)
-        return
+            logging.info("http request succeeded: " + state)
 
     def toggle_lab(self, state):
         """Toggle the state lab/on -> lab/off or lab/off -> lab/on."""
 
-        if (state == 0):
+        if state == 0:
             self.send_update("lab/off")
         else:
             self.send_update("lab/on")
-        return
 
     def run(self):
         """Starts the main loop."""
